e20863618e6bc9ff86248b73417cf3afbf02b8f0
tofan00 <fayyatfade@gmail.com>
2026-03-14 14:56:27 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,29 @@
+// firebase-config.js
+
+const firebaseConfig1885 = {
+  apiKey: "AIzaSyA_UmO_2zIQ4NOL6lgCwNzMImmjtcE9seA",
+  authDomain: "tofan-e55e7.firebaseapp.com",
+  databaseURL: "https://tofan-e55e7-default-rtdb.firebaseio.com",
+  projectId: "tofan-e55e7",
+  storageBucket: "tofan-e55e7.firebasestorage.app",
+  messagingSenderId: "158213758411",
+  appId: "1:158213758411:web:1e6b3050130a445832aa4e",
+  measurementId: "G-C1MP0758Z0"
+};
+
+const FIREBASE_PATHS_1885 = {
+  settingsStore: "settings1885/store1885",
+  settingsBanners: "settings1885/banners1885",
+  settingsPaymentMethods: "settings1885/paymentMethods1885",
+  categories: "categories1885",
+  products: "products1885",
+  users: "users1885",
+  orders: "orders1885"
+};
+
+const STORAGE_KEYS_1885 = {
+  cart: "tofan_cart_1885",
+  wishlist: "tofan_wishlist_1885",
+  checkoutSelection: "tofan_checkout_selection_1885",
+  selectedCategory: "tofan_selected_category_1885"
+};
